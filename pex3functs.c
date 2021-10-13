31922cd7ccb6aa9e8e21b0bfba7ac1aa1b4e6919
am17damienaf <c24damien.martinez@afacademy.af.edu>
2021-10-13 17:44:59 -0600

    Completion of clearGameBoard()

--- pex3functs.c
+++ pex3functs.c
@@ -5,7 +5,13 @@
 
 #include "pex3functs.h"
 
-void clearGameBoard(char board[6][6]);
+void clearGameBoard(char board[6][6]){
+    for (int i = 0; i < 6; ++i) {
+        for (int j = 0; j < 6; ++j) {
+            board[i][j] = '.';
+        }
+    }
+}
 
 void computerTakeTurn(char board[6][6]);
 
